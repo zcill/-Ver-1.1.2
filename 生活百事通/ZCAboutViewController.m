1eb834c1ce23c8e1df319e7eb5e787f84389dad8
zcill <zcill@icloud.com>
2015-11-10 09:25:32 +0800

    使用SFSafariViewController进行跳转到关注Github页面

--- 生活百事通/ZCAboutViewController.m
+++ 生活百事通/ZCAboutViewController.m
@@ -8,7 +8,9 @@
 
 #import "ZCAboutViewController.h"
 
-@interface ZCAboutViewController ()
+@import SafariServices;
+
+@interface ZCAboutViewController ()<SFSafariViewControllerDelegate, UIAlertViewDelegate>
 
 
 @end
@@ -32,16 +34,38 @@
 // 联系我们按钮
 - (IBAction)callMe:(id)sender {
     
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"呼叫" message:@"+86 186 1265 4171" delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"呼叫", nil];
     
+    [alertView show];
+    
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    
+    switch (buttonIndex) {
+        case 0:
+            return;
+        case 1:
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"tel://18612654171"]];
+            break;
+        default:
+            break;
+    }
     
 }
 
 // 关注作者按钮
 - (IBAction)gotoGithub:(id)sender {
     
-    // 使用safari打开github
-    NSURL *url = [NSURL URLWithString:@"https://github.com/zcill"];
-    [[UIApplication sharedApplication] openURL:url];
+    // 使用SFSafariViewController打开Github
+    NSString *urlString = @"https://github.com/zcill";
+    
+    SFSafariViewController *sfViewControllr = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:urlString]];
+    sfViewControllr.delegate = self;
+    
+    [self presentViewController:sfViewControllr animated:YES completion:^{
+        
+    }];
     
 }
 
